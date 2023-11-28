1163789f1bdd7ab34af7b926c4d72ff0af680028
Milka Zekarias <milkaz@Milkas-MacBook-Air.local>
2023-11-28 14:18:03 -0800

    adding final_game.py branch

---
+++ final_game.py
@@ -0,0 +1,76 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+
+red = (200,0,0)
+green = (0,200,0)
+
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+
+block_color = (53,115,255)
+
+## create game window
+screen = pygame.display.set_mode((500, 300))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def quitgame():
+    pygame.quit()
+    quit()
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(screen, ic,(x,y,w,h))
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    screen.blit(textSurf, textRect)
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        screen.fill(white)
+        largeText = pygame.font.SysFont("comicsansms",115)
+        TextSurf, TextRect = text_objects("A bit Racey", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        screen.blit(TextSurf, TextRect)
+
+        button("GO!",150,450,100,50,green,bright_green,game_loop)
+        button("Quit",550,450,100,50,red,bright_red,quitgame)
+        # TODO (Optional): You may implement three different difficulty levels.
+        # For example, level 1 is the default start speed and incrementing by 1.
+        # Levels 2 and 3 would have a higher start speed and larger increments.
+
+        pygame.display.update()
+        clock.tick(15)
+
+def game_loop()
+    ##TODO
